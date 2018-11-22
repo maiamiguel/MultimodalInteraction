c60c668a020a3ff501126aaee6815168f32c64f1
Nelson Costa <nelson.costa@ua.pt>
2018-11-22 23:15:45 +0000

    Added folder with Cleartrip example with basic grammar and no TTS

---
+++ Cleartrip/AppGui/AppGui/ProcessingCommandFunctions.cs
@@ -0,0 +1,134 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace AppGui
+{
+    class ProcessingCommandFunctions
+    {
+        private static Boolean firstTime = true;
+        private static IWebDriver driver;
+        private static Actions action;
+
+        public static void Search(String type, String city)
+        {
+            if (firstTime)
+            {
+                driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                //driver.Manage().Timeouts().ImplicitWait.Add(TimeSpan.FromSeconds(30));
+                firstTime = false;
+            }
+
+            if(type == "FLIGHT")
+            {
+                // go to home page
+                driver.Navigate().GoToUrl("https://www.cleartrip.com/");
+                // click country button
+                driver.FindElement(By.XPath("//*[@id='userAccountNav']/nav/ul/li[2]/a")).Click();
+                // select US country
+                driver.FindElement(By.XPath("//*[@id='countryForm']/li[8]/a")).Click();
+
+                switch (city)
+                {
+                    case "ROME":
+                        // send source (default)
+                        driver.FindElement(By.Id("FromTag")).Clear();
+                        driver.FindElement(By.Id("FromTag")).SendKeys("Lisbon, PT - Lisboa (LIS)");
+                        // send destination
+                        driver.FindElement(By.Id("ToTag")).Clear();
+                        driver.FindElement(By.Id("ToTag")).SendKeys("Rome, IT - Fiumicino (FCO)");
+                        break;
+                    case "PARIS":
+                        // send source (default)
+                        driver.FindElement(By.Id("FromTag")).Clear();
+                        driver.FindElement(By.Id("FromTag")).SendKeys("Lisbon, PT - Lisboa (LIS)");
+                        // send destination
+                        driver.FindElement(By.Id("ToTag")).Clear();
+                        driver.FindElement(By.Id("ToTag")).SendKeys("Paris, FR - Charles De Gaulle (CDG)");
+                        break;
+                }
+
+                // click depart box
+                driver.FindElement(By.XPath("//*[@id='DepartDate']")).Click();
+                // click next month button
+                driver.FindElement(By.XPath("//*[@id='ui-datepicker-div']/div[2]/div/a")).Click();
+                // select depart day
+                driver.FindElement(By.XPath("(//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr/td/a)[29]")).Click();
+                // click search button
+                driver.FindElement(By.Id("SearchBtn")).Click();
+            }
+            else if(type == "HOTEL")
+            {
+                // go to hotels page
+                driver.Navigate().GoToUrl("https://www.cleartrip.com/hotels/");
+                // click hotels link
+                //driver.FindElement(By.XPath("//*[@id='Home']/div/aside[1]/nav/ul[1]/li[2]/a")).Click();
+                // click country button
+                driver.FindElement(By.XPath("//*[@id='userAccountNav']/nav/ul/li[2]/a")).Click();
+                // select US country
+                driver.FindElement(By.XPath("//*[@id='countryForm']/li[8]/a")).Click();
+
+                switch (city)
+                {
+                    case "ROME":
+                        // send destination
+                        driver.FindElement(By.Name("from")).Click();
+                        driver.FindElement(By.Name("from")).SendKeys("Rome");
+                        // sleep 3 seconds
+                        Thread.Sleep(3000);
+                        // instantiate action object to press keys
+                        action = new Actions(driver);
+                        // we need to first press Down and Up keys, otherwise it won't work
+                        action.SendKeys(Keys.Down).Build().Perform();
+                        // sleep 3 seconds
+                        Thread.Sleep(3000);
+                        action.SendKeys(Keys.Up).Build().Perform();
+                        // sleep 3 seconds
+                        Thread.Sleep(3000);
+                        // press Enter key
+                        action.SendKeys(Keys.Enter).Build().Perform();
+                        break;
+                    case "PARIS":
+                        // send destination
+                        driver.FindElement(By.Name("from")).Click();
+                        driver.FindElement(By.Name("from")).SendKeys("Paris");
+                        // sleep 3 seconds
+                        Thread.Sleep(3000);
+                        // instantiate action object to press keys
+                        action = new Actions(driver);
+                        // we need to first press Down and Up keys, otherwise it won't work
+                        action.SendKeys(Keys.Down).Build().Perform();
+                        // sleep 3 seconds
+                        Thread.Sleep(3000);
+                        action.SendKeys(Keys.Up).Build().Perform();
+                        // sleep 3 seconds
+                        Thread.Sleep(3000);
+                        // press Enter key
+                        action.SendKeys(Keys.Enter).Build().Perform();
+                        break;
+                }
+
+                // click check-in box
+                driver.FindElement(By.XPath("//*[@id='CheckInDate']")).Click();
+                // click next month button
+                driver.FindElement(By.XPath("//*[@id='ui-datepicker-div']/div[2]/div/a")).Click();
+                // select check-in day
+                driver.FindElement(By.XPath("(//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr/td/a)[29]")).Click();
+                // click check-out box
+                driver.FindElement(By.XPath("//*[@id='CheckOutDate']")).Click();
+                // select check-out day
+                driver.FindElement(By.XPath("(//*[@id='ui-datepicker-div']/div[2]/table/tbody/tr/td/a)[1]")).Click();
+                // click search button
+                driver.FindElement(By.Id("SearchHotelsButton")).Click();
+            }
+        }
+    }
+}
---
+++ Cleartrip/speechModality/speechModality/SpeechMod.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mmisharp;
+using Microsoft.Speech.Recognition;
+using System.Timers;
+
+namespace speechModality{
+    public class SpeechMod{
+
+        private Tts tts;
+        private SpeechRecognitionEngine sre;
+        private Grammar gr;
+        public event EventHandler<SpeechEventArg> Recognized;
+
+        private static Boolean isAssistantSpeaking = false;
+
+        private int choice = 0;
+
+        protected virtual void onRecognized(SpeechEventArg msg){
+            EventHandler<SpeechEventArg> handler = Recognized;
+            if (handler != null){
+                handler(this, msg);
+            }
+        }
+
+        private LifeCycleEvents lce;
+        private MmiCommunication mmic;
+
+        public SpeechMod(){
+            //init LifeCycleEvents..
+            lce = new LifeCycleEvents("ASR", "FUSION","speech-1", "acoustic", "command"); // LifeCycleEvents(string source, string target, string id, string medium, string mode)
+            //mmic = new MmiCommunication("localhost",9876,"User1", "ASR");  //PORT TO FUSION - uncomment this line to work with fusion later
+            mmic = new MmiCommunication("localhost", 8000, "User1", "ASR"); // MmiCommunication(string IMhost, int portIM, string UserOD, string thisModalityName)
+
+            mmic.Send(lce.NewContextRequest());
+
+            //load pt recognizer
+            sre = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("pt-PT"));
+            gr = new Grammar(Environment.CurrentDirectory + "\\ptG.grxml", "rootRule");
+            sre.LoadGrammar(gr);
+            
+            sre.SetInputToDefaultAudioDevice();
+            sre.RecognizeAsync(RecognizeMode.Multiple);
+            sre.SpeechRecognized += Sre_SpeechRecognized;
+            sre.SpeechHypothesized += Sre_SpeechHypothesized;
+
+            // load speech synthetizer
+            tts = new Tts();
+
+            // introduce assistant
+            /*Speak( "Olá, eu sou o teu assistente de viagens." +
+                " Tenho todo o gosto em ajudar-te a planear as tuas férias de sonho." +
+                " Podes saber mais sobre mim dizendo: preciso de ajuda." );*/
+            Speak("Olá");
+        }
+
+        //TTS
+        //NOT USED
+        private void Speak(String text){
+            string str = "<speak version=\"1.0\"";
+            str += " xmlns:ssml=\"http://www.w3.org/2001/10/synthesis\"";
+            str += " xml:lang=\"pt-PT\">";
+            str += text;
+            str += "</speak>";
+
+            //Console.WriteLine("\nAssistant started speaking.");
+            //isAssistantSpeaking = true;
+
+            tts.Speak(str, 0);
+
+            //Console.WriteLine("\nAssistant stopped speaking.");
+            //isAssistantSpeaking = false;
+        }
+
+        //NOT USED
+        private void RandomSpeak(String[] choices)
+        {
+            Speak(choices[choice++ % choices.Length]);
+        }
+
+        private void Sre_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            onRecognized(new SpeechEventArg() { Text = e.Result.Text, Confidence = e.Result.Confidence, Final = false });
+        }
+
+        private void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            onRecognized(new SpeechEventArg() { Text = e.Result.Text, Confidence = e.Result.Confidence, Final = true });
+
+            //SEND
+            // IMPORTANT TO KEEP THE FORMAT {"recognized":["SHAPE","COLOR"]}
+            string json = "{ \"recognized\": [";
+            foreach (var resultSemantic in e.Result.Semantics)
+            {
+                json += "\"" + resultSemantic.Value.Value + "\", ";
+            }
+            json = json.Substring(0, json.Length - 2);
+            json += "] }";
+
+            var exNot = lce.ExtensionNotification(e.Result.Audio.StartTime + "", e.Result.Audio.StartTime.Add(e.Result.Audio.Duration) + "", e.Result.Confidence, json);
+            mmic.Send(exNot);
+        }
+    }
+}
